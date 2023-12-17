a482d3652d5d5c803c2c81ff7f36d27ee365d65a
philippesamuel <32560769+philippesamuel@users.noreply.github.com>
2023-12-17 15:26:33 +0000

    solve puzzle 2

--- day02/main.py
+++ day02/main.py
@@ -1,8 +1,12 @@
 """Day 2 puzzle of Advent of Code 2023."""
 import json
+from pprint import pprint
 from typing import Iterable, Iterator
 from day02.game import Game, Color, CubeSet
 from day02.parser import parse_game
+from functools import reduce
+from operator import mul
+
 
 CONSTRAINTS = {Color.RED: 12, Color.GREEN: 13, Color.BLUE: 14}
 
@@ -11,8 +15,17 @@ def main():
     """Day 2 puzzle of Advent of Code 2023"""
     file_path: str = "./data/day02_input.txt"
     games: Iterable[Game] = read_and_parse_games(file_path)
-    possible_games = filter(is_game_possible, games)
-    print(sum(g.id for g in possible_games))
+
+    # Part 1
+    # possible_games = filter(is_game_possible, games)
+    # print(sum(g.id for g in possible_games))
+
+    # Part 2
+    minimal_cube_sets = map(find_minimal_cube_set, games)
+    values = (tuple(s.values()) for s in minimal_cube_sets)
+    powers = (reduce(mul, s) for s in values)
+    # powers = (s[Color.RED] * s[Color.GREEN] * s[Color.BLUE] for s in minimal_cube_sets)
+    pprint(sum(powers))
 
 
 def read_games_json(file_path: str) -> Iterator[Game]:
@@ -41,6 +54,14 @@ def is_cube_set_possible(cube_set: CubeSet) -> bool:
         for color, constraint in CONSTRAINTS.items()
     )
 
+def find_minimal_cube_set(game: Game) -> CubeSet:
+    """Find the minimal cube set to make a game possible."""
+    minimal_cube_set = dict.fromkeys(Color, 0)
+    for cube_set in game.cube_sets:
+        for color, quantity in cube_set.items():
+            if quantity > minimal_cube_set[color]:
+                minimal_cube_set[color] = quantity
+    return minimal_cube_set
 
 if __name__ == "__main__":
     main()

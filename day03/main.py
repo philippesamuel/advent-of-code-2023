c36492ebfeb23514a4f45ebbc22d66beb9b6841a
philippesamuel <32560769+philippesamuel@users.noreply.github.com>
2023-12-20 23:47:55 +0000

    solve puzzle 3 part 1

--- day03/main.py
+++ day03/main.py
@@ -7,16 +7,18 @@ T = TypeVar("T")
 Triplet: TypeAlias = tuple[T, T, T]
 StrTriplet = Triplet[str]
 NUMBER_PATTERN = re.compile(r"\d+")
+SYMBOLS = ["#", "$", "%", "&", "*", "+", "-", "/", "=", "@"]
 SYMBOL_PATTERN = re.compile(r"\D")
+# SYMBOL_PATTERN = re.compile(r"(?!\n|\.|\d|\s)")
 
 
 def main() -> None:
     file_path: str = "./data/day03_input.txt"
     row_triplets: Iterable[StrTriplet] = read_row_triplets(file_path)
     numbers = map(find_numbers_with_adjacent_symbol, row_triplets)
-    for n in numbers:
-        print(n)
-    # print(sum(itertools.chain(*numbers)))
+    # for i, n in enumerate(numbers):
+    # print(i+1, n)
+    print(sum(itertools.chain(*numbers)))
 
 
 def read_row_triplets(file_path: str) -> Iterator[StrTriplet]:
@@ -59,22 +61,45 @@ def has_adjacent_symbol(span: tuple[int, int], row_triplet: StrTriplet) -> bool:
         current_row,
         next_row,
     ) = row_triplet
-    adjacent_cells = "".join(
-        (
-            *previous_row[left - 1 : right + 1],
-            current_row[left - 1],
-            current_row[right],
-            *next_row[left - 1 : right + 1],
+    if left == 0:
+        adjacent_cells = "".join(
+            (
+                *previous_row[: right + 1],
+                current_row[right],
+                *next_row[: right + 1],
+            )
         )
-    )
+    elif right == 140:
+        adjacent_cells = "".join(
+            (
+                *previous_row[left - 1 :],
+                current_row[left - 1],
+                *next_row[left - 1 :],
+            )
+        )
+    else:
+        adjacent_cells = "".join(
+            (
+                *previous_row[left - 1 : right + 1],
+                current_row[left - 1],
+                current_row[right],
+                *next_row[left - 1 : right + 1],
+            )
+        )
+
     return has_symbol(adjacent_cells)
 
 
 def has_symbol(cells: str) -> bool:
     """Check if any of the cells contain a symbol."""
-    clean_cells = cells.replace(".", "")
+    clean_cells = cells.replace(".", "").replace("\n", "")
     if SYMBOL_PATTERN.search(clean_cells):
+        # print(repr(clean_cells))
         return True
+    # print(repr(clean_cells))
+    # if any(symbol in cells for symbol in SYMBOLS):
+    #     return True
+    return False
 
 
 if __name__ == "__main__":
